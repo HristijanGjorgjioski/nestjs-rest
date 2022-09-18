b6a4c98990c119a39c2e8ad51f2ce28cd8eaa3e0
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-09-18 13:42:26 +0200

    Connect with DB and created services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
+import { ItemsModule } from './items/items.module';
+import { ItemsService } from './items/items.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    ItemsModule,
+  ],
   controllers: [AppController, ItemsController],
-  providers: [AppService],
+  providers: [AppService, ItemsService],
 })
 export class AppModule {}
